86bb05c178f4beb3b1b055944e99b294dca61031
alex sharamok <alexanders@uniform.dev>
2026-05-06 18:53:08 +0300

    match all

--- proxy.ts
+++ proxy.ts
@@ -4,7 +4,3 @@ import { NextResponse } from 'next/server'
 export function proxy() {
   return new NextResponse('Blocked by proxy', { status: 401 })
 }
-
-export const config = {
-  matcher: '/:path*',
-}
